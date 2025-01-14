fcf314d213b670d590d43b16f024301f8c28007d
modeledge <137086887+modeledge@users.noreply.github.com>
2025-01-14 14:57:31 -0700

    test json tag

--- gofs_test.go
+++ gofs_test.go
@@ -12,6 +12,12 @@ type HelloFuncParams struct {
 
 func hello(helloFuncParams HelloFuncParams) {}
 
+type HelloFuncParams2 struct {
+	world string `json:"worldParam"`
+}
+
+func hello2(helloFuncParams HelloFuncParams2) {}
+
 func Test(t *testing.T) {
 
 	details, err := gofs.GetFunctionDetails(hello)
@@ -26,4 +32,15 @@ func Test(t *testing.T) {
 		t.Errorf("Expected 'world', got %v", details.Parameters[0].Title)
 	}
 
+	details, err = gofs.GetFunctionDetails(hello2)
+	if err != nil {
+		t.Error(err)
+	}
+	if details.Name != "hello2" {
+		t.Errorf("Expected 'hello2', got %v", details)
+	}
+
+	if details.Parameters[0].Title != "worldParam" {
+		t.Errorf("Expected 'worldParam', got %v", details.Parameters[0].Title)
+	}
 }
